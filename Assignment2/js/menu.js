ed805cb543cebb6ef3d5c33180ecb59c08e54852
AlexC <aleksej.cornelissen@hotmail.com>
2022-03-11 15:09:08 +0100

    cool stuff

--- Assignment2/js/menu.js
+++ Assignment2/js/menu.js
@@ -27,35 +27,75 @@ class Menu{
         nav.appendChild(list);
         content.appendChild(nav);
     }
-}
 
-class MenuSection{
-    constructor(menuType){
-        this.menuType = menuType;
+    generateSections(){ //Generate HTML for each section
+        this.sections.forEach(section => {
+            section.generateItems();            
+        });
     }
 }
 
 class Food{
+    constructor(imageSrc="", name="", price=0, spiciness = -1, calories = 0, vegan = false){
+        this.name = name;
+        this.price = price;
+        this.spiciness = spiciness;
+        this.calories = calories;
+        this.vegan = vegan;
+        this.imageSrc = imageSrc;
+   
+       }
+   
+       generateStuff(){
+           //do HTML DOM stuff
+           var image = document.createElement("IMG");
+           image.setAttribute("src", this.imageSrc);
+           image.setAttribute("alt", this.name);
+           content.appendChild(image);
+       }
+   }
 
+class MenuSection{
+    constructor(menuType){
+        this.menuType = menuType;
+        this.foods = [];
+    }
+
+    generateItems(){ //Generate HTML for each item in this section
+        for (const food of this.foods){
+            food.generateStuff();
+        }
+    }
 }
 
-class Burger extends Food{
 
+
+class Burger extends Food{
+    constructor(...params){
+        super(...params)
+    }
 }
 
 class Chicken extends Food{
-
+    constructor(...params){
+        super(...params)
+    }
 }
 
 class Drinks extends Food{
-
+    constructor(...params){
+        super(...params)
+    }
 }
 
-burgerSection = new MenuSection("Chicken");
+burgerSection = new MenuSection("Chicken"); //mmm sjiken
 chickenSection = new MenuSection("Burgers");
 drinkSection = new MenuSection("Drinks");
 
 menu = new Menu();
 menu.sections = [burgerSection, chickenSection, drinkSection];
 
+
+burgerSection.foods.push(new Burger("resources/Menu-Burgers/B1.jpg")); 
 menu.generateNav();
+menu.generateSections();
