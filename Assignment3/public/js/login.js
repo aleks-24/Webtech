e51eb850c759c99c4ca5e1ea61f4ea8d3d4bc47a
Astavie <astavie@protonmail.com>
2022-04-15 16:35:02 +0200

    fix login

---
+++ Assignment3/public/js/login.js
@@ -0,0 +1,39 @@
+async function login(e) {
+    console.log(e);
+
+    const data = {};
+    data.username = document.getElementById("loginUsername").value;
+    data.password = document.getElementById("loginPassword").value;
+
+    const ret = await fetch('/api/auth', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    console.log(ret);
+}
+
+async function register() {
+    const data = {};
+    data.username = document.getElementById("username").value;
+    data.password = document.getElementById("password").value;
+    data.firstname = document.getElementById("firstName").value;
+    data.lastname = document.getElementById("lastName").value;
+    data.email = document.getElementById("emailAdress").value;
+    data.address = document.getElementById("adress").value;
+    data.phonenumber = document.getElementById("phoneNumber").value;
+    data.postcode = document.getElementById("postalCode").value;
+
+    const ret = await fetch('/api/user', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    console.log(ret);
+}
